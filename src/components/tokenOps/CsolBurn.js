3df40c8879cd84f2555a48d9ccf876dac9061880
abdulhakim-altunkaya <altunkaya2@gmail.com>
2024-01-26 19:11:09 +0300

    csol token burn, balance and free minting finished

--- src/components/tokenOps/CsolBurn.js
+++ src/components/tokenOps/CsolBurn.js
@@ -2,8 +2,34 @@ import React, { useState } from 'react';
 import { useAccount } from '../../Store';
 
 function CsolBurn() {
+  const contractCSOL = useAccount(state => state.contractCsol2);
+  let [amount, setAmount] = useState("");
+  let [message, setMessage] = useState("");
+
+  const burnToken = async () => {
+    let amount1 = parseInt(amount);
+    if (amount1 < 1 || amount1 === "") {
+      alert("please enter a valid amount");
+      return;
+    }
+
+    let userBalance = await contractCSOL.getYourTokenBalance();
+    let userBalance2 = userBalance.toString();
+    let userBalance3 = parseInt(userBalance);
+    if(userBalance3 < 1) {
+      alert("you dont have CSOL to burn");
+      return;
+    }
+    await contractCSOL.burnToken(amount1);
+    setMessage("success, you burned", amount1, "tokens");
+  }
+
   return (
-    <div>CsolBurn</div>
+    <div>
+      <button className='button4' onClick={burnToken}>Burn CSOL</button>
+      <input type='number' className='inputFields' placeholder='enter amount'
+        value={amount} onChange={ e => setAmount(e.target.value)} /> {message}
+    </div>
   )
 }
 
