ac765b8cd2e04d9d579c38f875859fde23f94225
abdulhakim-altunkaya <altunkaya2@gmail.com>
2024-02-17 13:34:16 +0300

    paying fee component finished

--- src/components/mixingOps/PayFee.js
+++ src/components/mixingOps/PayFee.js
@@ -4,40 +4,51 @@ import { useSelector } from "react-redux";
 //getting contract from zustand store
 import { useAccount } from '../../Store';
 
-function PayFee() {
-
+function PayFee() {  
   //fetching user account from redux storage. 
   const userAccount2 = useSelector( (state) => state.userAccount);
-
   //fetching closio contract from zustand storage
   const contractClosio = useAccount(state => state.contractClosio2);
 
   let [message, setMessage] = useState("");
 
   const payFee = async () => {
-    //check 1: if user has metamask installed on browser
-    if(window.ethereum === "undefined") {
-      alert("Please install Metamask to your Browser");
-      return;
+    try {
+      //check 1: if user has metamask installed on browser
+      if(window.ethereum === "undefined") {
+        alert("Please install Metamask to your Browser");
+        return;
+      }
+      //check 2: if user has signed in or not
+      if (userAccount2 === "undefined" || userAccount2 === "") {
+        alert("Please sign in to website. Go to Token Operations section and click on Connect Metamask button.");
+        return;
+      }
+      //check 3: if user has paid or not
+      let paymentStatus = await contractClosio.feePayers(userAccount2);
+      if(paymentStatus === true) {
+        alert("You already have paid fee. You dont need to pay again");
+        return;
+      }
+      //execution
+      const tx = await contractClosio.payFee();
+      await tx.wait();
+      setMessage("You successfully paid service fee");
+    } catch (error) {
+      // Check if the error contains the "transaction" field
+      if (error.transaction && error.transaction.from) {
+        // Log the error.message field
+        console.error('Error Message:', error.error.data.message);
+        alert("Paying fee failed. Check your Metamask and internet connection, refresh the page and try again");
+        setMessage(error.error.data.message);
+      } else {
+        // Log all error message
+        console.error(error);
+      }
     }
-    //check 2: if user has signed in or not
-    if (userAccount2 === "undefined" || userAccount2 === "") {
-      alert("Please sign in to website. Go to Token Operations section and click on Connect Metamask button.");
-      return;
-    }
-    //check 3: if user has paid or not
-    let paymentStatus = await contractClosio.feePayers(userAccount2);
-    if(paymentStatus === true) {
-      alert("You already have paid fee. You dont need to pay again");
-      return;
-    }
-
-    //execution
-    const tx = await contractClosio.payFee();
-    await tx.wait();
-    setMessage("You successfully paid service fee");
 
   }
+
   return (
     <div>
         <button className='button10' onClick={payFee}>Pay Fee</button> {message}
