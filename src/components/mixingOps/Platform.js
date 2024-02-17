9d94b52c691014e7cb88555833ae4bb830cbf162
abdulhakim-altunkaya <altunkaya2@gmail.com>
2024-02-17 14:24:02 +0300

    withdraw all component finished with further need to test if frontend can capture two different bools from contract

--- src/components/mixingOps/Platform.js
+++ src/components/mixingOps/Platform.js
@@ -25,25 +25,3 @@ function Platform() {
 }
 
 export default Platform;
-
-/*
-      GET Balances component: 
-    function withdrawPart(string calldata _privateWord, bytes32 _newHash, address _receiver, uint _amount) 
-    external nonReentrant isPaused hasPaid returns(bool) 
-    {
-      
-      function withdrawAll(string calldata _privateWord, address _receiver) external nonReentrant isPaused hasPaid returns(bool) {
-            
-      approval fee CSOL
-      approval WETH 
-
-      function payFee() public {
-      function deposit(bytes32 _hash, uint _amount) external isPaused hasPaid nonReentrant returns(string memory) {
-      
-      function createHashSalty(string calldata _privateWord) external pure returns(bytes32) {
-
-      
-      
-      
-      
-      */
--- src/components/mixingOps/WithdrawAll.js
+++ src/components/mixingOps/WithdrawAll.js
@@ -16,52 +16,69 @@ function WithdrawAll() {
 
   //fetching closio and csol contracts from zustand storage
   const contractClosio = useAccount(state => state.contractClosio2);
-  const contractCSOL = useAccount(state => state.contractCsol2);
 
   let [message, setMessage] = useState("");
   let [privateWord, setPrivateWord] = useState("");
   let [receiver, setReceiver] = useState("");
 
   const withdrawAll = async () => {
-    //check 1: if user has metamask installed on browser
-    if(window.ethereum === "undefined") {
-      alert("Please install Metamask to your Browser");
-      return;
+    try {
+      //check 1: if user has metamask installed on browser
+      if(window.ethereum === "undefined") {
+        alert("Please install Metamask to your Browser");
+        return;
+      }
+      //check 2: if user has signed in or not
+      if (userAccount2 === "undefined" || userAccount2 === "") {
+        alert("Please sign in to website. Go to Token Operations section and click on Connect Metamask button.");
+        return;
+      }
+      //check 3: if private word or receiver input is empty
+      if(privateWord === "" || receiver === "") {
+        alert("You cannot leave input areas empty");
+        return
+      }
+      //check 4: if receiver address is valid
+      if(receiver.length < 39) {
+        alert("invalid address length");
+        return;
+      } else if(receiver.slice(0, 2) !== "0x") {
+        alert("invalid hash");
+        return;
+      }
+      //check 5: if user has paid service fee
+      let feePaymentStatus = await contractClosio.feePayers(userAccount2);
+      if(feePaymentStatus === false) {
+        alert("You need to pay fee. Each time you call deposit, withdraw all or withdraw part functions, it will cost you 1 CSOL");
+        return;
+      }
+      //check : if system is paused
+      let isSystemPaused = await contractClosio.pauseContract();
+      if(isSystemPaused === true) {
+        alert("System has been paused by Owner. Contact him to unpause: drysoftware1@gmail.com");
+        return;
+      }   
+      //execution
+      let tx = await contractClosio.withdrawAll(privateWord, receiver);
+      await tx.wait();
+      if(tx === false) {
+        alert("Probably old/wrong private word error because there is 0 balance. Check your private word. Then pay fee again. And then call deposit function.");
+        return;
+      } else {
+        setMessage(`You successfully withdraw all balance.`);
+      }
+    } catch (error) {
+      // Check if the error contains the "transaction" field
+      if (error.transaction && error.transaction.from) {
+        // Log the error.message field
+        console.error('Error Message:', error.error.data.message);
+        alert("Withdraw All balance failed. Check your private word. Check your Metamask and internet connection, refresh the page and try again");
+        setMessage(error.error.data.message);
+      } else {
+        // Log all error message
+        console.error(error);
+      }
     }
-    //check 2: if user has signed in or not
-    if (userAccount2 === "undefined" || userAccount2 === "") {
-      alert("Please sign in to website. Go to Token Operations section and click on Connect Metamask button.");
-      return;
-    }
-    //check 3: if private word or receiver input is empty
-    if(privateWord === "" || receiver === "") {
-      alert("You cannot leave input areas empty");
-      return
-    }
-    //check 4: if receiver address is valid
-    if(receiver.length < 39) {
-      alert("invalid address length");
-      return;
-    } else if(receiver.slice(0, 2) !== "0x") {
-      alert("invalid hash");
-      return;
-    }
-    //check 5: if user has paid service fee
-    let feePaymentStatus = await contractClosio.feePayers(userAccount2);
-    if(feePaymentStatus === false) {
-      alert("You need to pay fee. Each time you call deposit, withdraw all or withdraw part functions, it will cost you 1 CSOL");
-      return;
-    }
-    //check : if system is paused
-    let isSystemPaused = await contractClosio.pauseContract();
-    if(isSystemPaused === true) {
-      alert("System has been paused by Owner. Contact him to unpause: drysoftware1@gmail.com");
-      return;
-    }   
-    //execution
-    let exeResult = await contractClosio.withdrawAll(privateWord, receiver);
-    await exeResult.wait();
-    setMessage("Balance withdrawal successful");
     
   }
   return (
